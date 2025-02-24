a2ea464995532c4c958b21806e0fc344ff9449c3
Alex Ziskind <alex@nuvious.com>
2025-02-24 17:49:34 -0500

    refactor dashboard styles

--- src/modules/dashboard/pages/dashboard/dashboard-page.tsx
+++ src/modules/dashboard/pages/dashboard/dashboard-page.tsx
@@ -1,6 +1,8 @@
 import { useContext, useState } from "react";
 import { useQuery } from "react-query";
 
+import "./dashboard-page.css"; // <-- New stylesheet import
+
 import { DashboardFilter } from "../../repositories/dashboard.repository";
 import { formatDateEnUs } from "../../../../core/helpers/date-utils";
 import { ActiveIssuesComponent } from "../../components/active-issues/active-issues";
@@ -60,27 +62,14 @@ export function DashboardPage() {
   }
 
   return (
-    <div className="dashboard">
+    <div className="dashboard-page">
       {/* Header Section */}
       <div className="container">
         <div className="row align-items-center justify-content-between">
           {/* Left: Dashboard Title */}
           <div className="col-auto">
-            <div
-              className="Frame13 d-flex flex-column align-items-start gap-2"
-              style={{ width: "700px" }}
-            >
-              <div
-                className="Label text-center"
-                style={{
-                  color: "#3D3D3D",
-                  fontSize: "40px",
-                  fontFamily: "Roboto",
-                  fontWeight: 500,
-                }}
-              >
-                Dashboard
-              </div>
+            <div className="frame13 d-flex flex-column align-items-start gap-2">
+              <div className="dashboard-title text-center">Dashboard</div>
             </div>
           </div>
 
@@ -106,129 +95,27 @@ export function DashboardPage() {
       </div>
 
       {/* Diagram and Statistics Section */}
-      <div
-        className="DiagramAndStatistics"
-        style={{
-          height: "525px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          gap: "16px",
-        }}
-      >
-        {/* Active Issues (Styled similarly to Angular’s <app-active-issues> element) */}
-        <div
-          className="Statistics"
-          style={{
-            alignSelf: "stretch",
-            paddingLeft: "192px",
-            paddingRight: "192px",
-            paddingTop: "16px",
-            paddingBottom: "16px",
-            background: "#FAFAFA",
-            borderRadius: "4px",
-            justifyContent: "space-between",
-            alignItems: "center",
-            display: "inline-flex",
-          }}
-        >
+      <div className="DiagramAndStatistics">
+        {/* Active Issues */}
+        <div className="Statistics StatisticsBlock">
           <ActiveIssuesComponent statusCounts={statusCounts} />
         </div>
 
         {/* Additional Statistics Section */}
-        <div
-          className="Frame36"
-          style={{
-            alignSelf: "stretch",
-            height: "405px",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            alignItems: "flex-end",
-            gap: "8px",
-          }}
-        >
-          <div
-            className="Frame38"
-            style={{
-              alignSelf: "stretch",
-              display: "inline-flex",
-              justifyContent: "space-between",
-              alignItems: "flex-start",
-            }}
-          >
+        <div className="frame36 stretch-height">
+          <div className="frame38">
             {/* Left Statistics Block */}
-            <div
-              className="Statistics"
-              style={{
-                width: "218px",
-                display: "inline-flex",
-                flexDirection: "column",
-                justifyContent: "flex-start",
-                alignItems: "flex-start",
-              }}
-            >
-              <div
-                className="Label"
-                style={{
-                  display: "inline-flex",
-                  justifyContent: "center",
-                  alignItems: "flex-start",
-                }}
-              >
-                <div
-                  className="BaseInputLabel"
-                  style={{
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    gap: "6px",
-                  }}
-                >
-                  <div
-                    className="Label"
-                    style={{
-                      color: "#3D3D3D",
-                      fontSize: "21px",
-                      fontFamily: "Roboto",
-                      fontWeight: 700,
-                      lineHeight: "27.93px",
-                      wordWrap: "break-word",
-                    }}
-                  >
+            <div className="Statistics left-stat-block">
+              <div className="Label inline-flex">
+                <div className="BaseInputLabel gap-6">
+                  <div className="statistics-heading">
                     All Issues
                   </div>
                 </div>
               </div>
-              <div
-                className="Label"
-                style={{
-                  display: "inline-flex",
-                  justifyContent: "center",
-                  alignItems: "flex-start",
-                }}
-              >
-                <div
-                  className="BaseInputLabel"
-                  style={{
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    gap: "6px",
-                  }}
-                >
-                  <div
-                    className="Label"
-                    style={{
-                      color: "#3D3D3D",
-                      fontSize: "16px",
-                      fontFamily: "Roboto",
-                      fontWeight: 400,
-                      lineHeight: "24px",
-                      wordWrap: "break-word",
-                    }}
-                  >
+              <div className="Label inline-flex">
+                <div className="BaseInputLabel gap-6">
+                  <div className="statistics-subheading">
                     Active Issues
                   </div>
                 </div>
@@ -236,77 +123,17 @@ export function DashboardPage() {
             </div>
 
             {/* Right Statistics Block */}
-            <div
-              className="Statistics"
-              style={{
-                width: "218px",
-                alignSelf: "stretch",
-                display: "inline-flex",
-                flexDirection: "column",
-                justifyContent: "space-between",
-                alignItems: "flex-end",
-              }}
-            >
-              <div
-                className="Label"
-                style={{
-                  display: "inline-flex",
-                  justifyContent: "center",
-                  alignItems: "flex-start",
-                }}
-              >
-                <div
-                  className="BaseInputLabel"
-                  style={{
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    gap: "6px",
-                  }}
-                >
-                  <div
-                    className="Label"
-                    style={{
-                      color: "#3D3D3D",
-                      fontSize: "16px",
-                      fontFamily: "Roboto",
-                      fontWeight: 400,
-                      lineHeight: "24px",
-                      wordWrap: "break-word",
-                    }}
-                  >
+            <div className="Statistics right-stat-block">
+              <div className="Label inline-flex">
+                <div className="BaseInputLabel gap-6">
+                  <div className="statistics-subheading">
                     Highest: 100% on Oct 11, 2018
                   </div>
                 </div>
               </div>
-              <div
-                className="Label"
-                style={{
-                  display: "inline-flex",
-                  justifyContent: "center",
-                  alignItems: "flex-start",
-                }}
-              >
-                <div
-                  className="BaseInputLabel"
-                  style={{
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    gap: "6px",
-                  }}
-                >
-                  <div
-                    className="Label"
-                    style={{
-                      color: "#3D3D3D",
-                      fontSize: "16px",
-                      fontFamily: "Roboto",
-                      fontWeight: 400,
-                      lineHeight: "24px",
-                      wordWrap: "break-word",
-                    }}
-                  >
+              <div className="Label inline-flex">
+                <div className="BaseInputLabel gap-6">
+                  <div className="statistics-subheading">
                     Lowest: 20% on Oct 9, 2018
                   </div>
                 </div>
@@ -316,7 +143,7 @@ export function DashboardPage() {
         </div>
 
         {/* Chart Section (Placeholder) */}
-        <div className="Frame36" style={{ display: "block", width: "100%" }}>
+        <div className="frame36 chart-section">
           {/* Insert your chart component or markup here */}
         </div>
       </div>
