be8694574c963a456c0aee0ca4c2405c21ef3eb2
Alex Ziskind <alex@nuvious.com>
2025-03-13 15:43:50 -0400

    migrate to vite

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-
-const root = createRoot(document.getElementById("root") as HTMLElement);
-
-root.render(<App />);
-
